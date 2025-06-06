f59273d78831c13435ac6500d163459996620aa6
Anthony Correa <a@correa.co>
2025-06-06 12:22:33 -0500

    rename readme as markdwon, update manifest

--- tonys-sportspress-enhancements.php
+++ tonys-sportspress-enhancements.php
@@ -1,10 +1,10 @@
 <?php
 /**
  * Plugin Name:     Tonys SportsPress Enhancements
- * Plugin URI:      PLUGIN SITE HERE
+ * Plugin URI:      https://github.com/anthonyscorrea/tonys-sportspress-enhancements
  * Description:     Suite of SportsPress Enhancements
- * Author:          YOUR NAME HERE
- * Author URI:      YOUR SITE HERE
+ * Author:          Tony Correa
+ * Author URI:      Yhttps://github.com/anthonyscorrea/
  * Text Domain:     tonys-sportspress-enhancements
  * Domain Path:     /languages
  * Version:         0.1.1
